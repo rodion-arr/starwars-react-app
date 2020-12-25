53f57507ff61f89251989994df9808f2ac3ebd26
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-12-25 16:36:24 +0200

    Add header layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import { ReactComponent as SearchIcon } from './images/header/search.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="sw-react-app">
+        <div className="header">
+          <div className="header__menu burger-menu">
+            <div className="burger-menu__bar" />
+            <div className="burger-menu__bar" />
+            <div className="burger-menu__bar" />
+          </div>
+          <div className="header__logo" />
+          <div className="header__search search">
+            <SearchIcon />
+          </div>
+        </div>
     </div>
   );
 }
