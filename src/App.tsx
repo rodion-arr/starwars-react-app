e2e9c0b4d5c2a6315c9d1dd0f7014439be87b5aa
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-12-26 15:08:15 +0200

    Add home banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import './App.scss';
-import { ReactComponent as SearchIcon } from './images/header/search.svg';
+import {ReactComponent as SearchIcon} from './images/header/search.svg';
 
 function App() {
   return (
     <div className="sw-react-app">
-        <div className="header">
-          <div className="header__menu burger-menu">
-            <div className="burger-menu__bar" />
-            <div className="burger-menu__bar" />
-            <div className="burger-menu__bar" />
-          </div>
-          <div className="header__logo" />
-          <div className="header__search search">
-            <SearchIcon />
-          </div>
+      <div className="header">
+        <div className="header__menu burger-menu">
+          <div className="burger-menu__bar"/>
+          <div className="burger-menu__bar"/>
+          <div className="burger-menu__bar"/>
         </div>
+        <div className="header__logo"/>
+        <div className="header__search search">
+          <SearchIcon/>
+        </div>
+      </div>
+      <div className="wrapper">
+        <div className="home-banner">
+          <div className="home-banner__logo"/>
+          <div className="home-banner__text">Welcome to StarWars catalog!</div>
+        </div>
+      </div>
+      <div className="footer">
+        footer
+      </div>
     </div>
   );
 }
