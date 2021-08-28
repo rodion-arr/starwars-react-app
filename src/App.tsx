9c1f0850eeeb030cbad78ae9d9e593974faa9a5e
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2021-08-29 00:08:42 +0300

    Add catalog and footer blocks to layout

--- src/App.tsx
+++ src/App.tsx
@@ -21,9 +21,38 @@ function App() {
           <div className="home-banner__logo"/>
           <div className="home-banner__text">Welcome to StarWars catalog!</div>
         </div>
+        <div className="catalog">
+          <h2 className="catalog__title">People</h2>
+          <div className="catalog__items catalog-items-wrap">
+            <div className="catalog-item">
+              <a href="#" className="catalog-item__link">
+              <div className="catalog-item__img">
+                <img src="/assets/content/people/1.png" alt="Luke"/>
+              </div>
+              <div className="catalog-item__info catalog-item-info">
+                <div className="catalog-item-info__name">Luke Skywalker</div>
+              </div>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="footer">
-        footer
+        <div className="social-links">
+          <p className="social-links__text">Follow:</p>
+          <ul className="social-links-list">
+            <li className="social-links-list__item">
+              <a className="social-links-list__item--linkedin icon-wrap" href="https://www.linkedin.com/in/rodionabdurakhimov/" target="_blank" rel="noopener noreferrer" title="Rodion Abdurakhimov | LinkedIn">
+                <i className="social-links-list__icon" />
+              </a>
+            </li>
+            <li className="social-links-list__item">
+              <a className="social-links-list__item--github icon-wrap" href="https://github.com/rodion-arr" target="_blank" rel="noopener noreferrer" title="Rodion Abdurakhimov | GitHub">
+                <i className="social-links-list__icon" />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
