2a0bc60a9ec998b17d7210b1b23bfb1f611b3951
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2021-10-14 12:42:09 +0300

    Add mock for useDb() hook

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,8 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import {BrowserRouter as Router} from "react-router-dom";
 
+jest.mock('./hooks/useDb');
+
 test('renders welcome', () => {
   render(<Router><App /></Router>);
   const linkElement = screen.getByText(/people/i);
