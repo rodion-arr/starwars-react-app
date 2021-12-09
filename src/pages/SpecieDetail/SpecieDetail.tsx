e6f9b4693d0e84aafdc98edf1ca1595717053981
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2021-12-09 19:16:56 +0200

    fix(SpecieDetail): Fixes empty homeword case
    Fixes #183

--- src/pages/SpecieDetail/SpecieDetail.tsx
+++ src/pages/SpecieDetail/SpecieDetail.tsx
@@ -20,8 +20,10 @@ export function SpecieDetail({
   }
 
   const entity = species[id];
+  console.log(planets);
+  console.log(entity.homeworld);
   const image = DbService.getImageUrl(entity);
-  const entityProps = {
+  const entityProps: Record<string, string | JSX.Element> = {
     'classification': entity.classification,
     'designation': entity.designation,
     'average height': entity.average_height,
@@ -29,7 +31,10 @@ export function SpecieDetail({
     'hair colors': entity.hair_colors,
     'eye colors': entity.eye_colors,
     'average lifespan': entity.average_lifespan,
-    'homeworld': <Link to={URI.planets.detail.replace(':id', entity.homeworld)}>{planets[entity.homeworld].name}</Link>,
+  }
+
+  if (entity.homeworld) {
+    entityProps.homeworld = <Link to={URI.planets.detail.replace(':id', entity.homeworld)}>{planets[entity.homeworld].name}</Link>;
   }
 
   return (
