07619705da7e547602ba14af5a59e508e80638ff
Rodion Abdurakhimov <rodion-arr@users.noreply.github.com>
2022-09-21 13:26:24 +0300

    Timeline fixes (#363)
    * Update boba fett position
    * Add chart description and fix extra columns
    * Add collapse functionality for first col
    * Cover delta functionality

--- src/components/Timeline/TimelineChart/__tests__/TimelineChart.test.tsx
+++ src/components/Timeline/TimelineChart/__tests__/TimelineChart.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { TimelineChart } from "../TimelineChart";
 
 describe("TimelineChart", () => {
@@ -16,9 +16,39 @@ describe("TimelineChart", () => {
             start: -9,
             end: -9,
           },
+          "1": {
+            id: "1",
+            title: "Episode 2",
+            start: 1,
+            end: 10,
+          },
         }}
       />
     );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should have collapse functionality", function () {
+    const { container } = render(
+      <TimelineChart
+        range={{
+          min: -10,
+          max: 10,
+        }}
+        timeline={{
+          "0": {
+            id: "0",
+            title: "The Phantom Menace",
+            start: -9,
+            end: -9,
+          },
+        }}
+      />
+    );
+
+    const collapseButton = container.getElementsByClassName('timeline__collapse')[0];
+    fireEvent.click(collapseButton);
 
     expect(container).toMatchSnapshot();
   });
